365154bcf6fed5d2a4389b34b94974dc924cd048
NEELA SAIKIRAN <neelasaikiran123@gmail.com>
2024-08-25 21:25:51 +0530

    Added script

--- script.js
+++ script.js
@@ -1,60 +1,53 @@
 let equal_pressed = 0;
+//Refer all buttons excluding AC and DEL
+let button_input = document.querySelectorAll(".input-button");
+//Refer input,equal,clear and erase
+let input = document.getElementById("input");
+let equal = document.getElementById("equal");
+let clear = document.getElementById("clear");
+let erase = document.getElementById("erase");
 
-// Refer all buttons excluding AC and DEL
-let button_input = document.querySelectorAll('.input-button');
-
-// Refer input, equal, clear, and erase
-let input = document.getElementById('input');
-let equal = document.getElementById('equal');
-let clear = document.getElementById('clear');
-let erase = document.getElementById('erase');
-
-// Clear input on page load
 window.onload = () => {
-    input.value = '';
+  input.value = "";
 };
 
-// Access each class using forEach
-button_input.forEach(button => {
-    button.addEventListener("click", () => {
-        if (equal_pressed == 1) {
-            input.value = '';
-            equal_pressed = 0;
-        }
-
-        // Display value of each button
-        input.value += button.value;
-    });
-});
-
-// Solve the user's input when clicked on equal sign
-equal.addEventListener("click", () => {
-    let input_val = input.value;
-
-    try {
-        // evaluate user's input
-        let solution = eval(input_val);
-
-        if (solution === undefined || isNaN(solution)) {
-            alert("Invalid Input!");
-            input.value = "";
-        } else {
-            input.value = solution;
-        }
-
-        equal_pressed = 1; // Mark equal_pressed after evaluation
-    } catch (err) {
-        alert("Invalid Input!");
-        input.value = "";
+//Access each class using forEach
+button_input.forEach((button_class) => {
+  button_class.addEventListener("click", () => {
+    if (equal_pressed == 1) {
+      input.value = "";
+      equal_pressed = 0;
     }
+    //display value of each button
+    input.value += button_class.value;
+  });
 });
 
-// Clear input on 'AC' button click
+//Solve the user's input when clicked on equal sign
+equal.addEventListener("click", () => {
+  equal_pressed = 1;
+  let inp_val = input.value;
+  try {
+    //evaluate user's input
+    let solution = eval(inp_val);
+    //True for natural numbers
+    //false for decimals
+    if (Number.isInteger(solution)) {
+      input.value = solution;
+    } else {
+      input.value = solution.toFixed(2);
+    }
+  } catch (err) {
+    //If user has entered invalid input
+    alert("Invalid Input");
+  }
+});
+
+//Clear Whole Input
 clear.addEventListener("click", () => {
-    input.value = "";
+  input.value = "";
 });
-
-// Erase the last character on 'DEL' button click
+//Erase Single Digit
 erase.addEventListener("click", () => {
-    input.value = input.value.slice(0, -1);
+  input.value = input.value.substr(0, input.value.length - 1);
 });
